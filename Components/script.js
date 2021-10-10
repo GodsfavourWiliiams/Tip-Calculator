9e7b6831e0dc4db3578589558a9a5ef47c1c715e
GodzFeivur <godzfeivvur@gmail.com>
2021-10-10 18:22:22 +0100

    refurblish app

--- Components/script.js
+++ Components/script.js
@@ -1,73 +1,110 @@
-let TipAmount = document.getElementById('result-Amounts'); // Tip 
-let Total = document.getElementById('results'); // Tip
-let inputVal = document.getElementById('bill-display'); // input bill
-let inputVals = document.getElementById('number-displays'); // input number of people
-// let Reset = docue.getElementById("Reset")
-let FivePer = 0.5;
-let TenPer = 0.10;
-let FifteenPer = 0.15;
-let TwentyFivePer = 0.25;
-let FiftyPer = 0.50;
-let Custom = document.getElementById('custom');
+"use strict";
 
-let TotalPercentage = 1.15
+// UI elements
+const bill = document.querySelector(".bill");
+const labelSelectTip = document.querySelectorAll(".btn");
+const customTip = document.querySelector("#custom-tip");
+const persons = document.querySelector(".select-persons");
+const billError = document.querySelector(".bill-error");
+const personError = document.querySelector(".person-error");
+const tipAmount = document.querySelector(".tip-amount");
+const totalAmount = document.querySelector(".total-amount");
+const btnReset = document.querySelector(".btn-reset");
 
+// init
+function init() {
+    // clear input fields
+    bill.value = persons.value = customTip.value = "";
 
-function calc() {
-    if (inputVal.value == 0) {
-        document.getElementById('error-message').textContent = "";
-    } else {
-        //DO SOMETHING
+    // zero tips
+    tipAmount.textContent = totalAmount.textContent = "$0.00";
+}
+init();
+
+// showError when inputs is empty or zero
+function showError(message, type) {
+    message.textContent = "Can't be zero";
+    type.classList.add("error");
+
+    // disapear the message after 3 sec
+    setTimeout(function disApear() {
+        message.textContent = "";
+        type.classList.remove("error");
+    }, 1000);
+}
+
+// remove background color
+function removeBackground() {
+    labelSelectTip.forEach((btn) => btn.classList.remove("add"));
+}
+
+// calcTip
+function calcTip(tipType) {
+    // convert bill and person amount string into number
+    const billNum = Number(bill.value);
+    const personsNum = Number(persons.value);
+
+    // check: if bill input is empty warn it
+    if (bill.value === "" || billNum <= 0) {
+        showError(billError, bill);
     }
-    if (inputVals.value < 1) {
-        document.getElementById('error-messages').textContent = "Can't be Zero";
-    } else {
-        //do this
+
+    if (persons.value === "" || personsNum <= 0) {
+        showError(personError, persons);
     }
-    let Sumtotals = inputVal.value * FifteenPer
-    let SumTotals = Sumtotals / inputVals.value
-    let SumTotal = inputVal.value * Custom.value / inputVals.value;
-    let TotalPerson = inputVal.value * TotalPercentage / inputVals.value
-    document.getElementById('result-Amounts').textContent = SumTotal.toFixed(2);
-    document.getElementById('result-Amounts').textContent = SumTotals.toFixed(2);
-    document.getElementById('results').textContent = TotalPerson.toFixed(2);
 
+    if (
+        bill.value !== "" &&
+        billNum >= 0 &&
+        persons.value !== "" &&
+        personsNum >= 0
+    ) {
+        // calculate the %
+        const percent = (billNum / 100) * Number(tipType.value);
+
+        // calculate per person tip
+        const perPerson = percent / personsNum;
+
+        // calculate per person bill
+        const perPersonBill = (billNum + percent) / personsNum;
+
+        tipAmount.innerHTML = `$${perPerson.toFixed(2)}`;
+        totalAmount.innerHTML = `$${perPersonBill.toFixed(2)}`;
+    }
 }
 
-function point1() {
-    Sumtotals = inputVal.value * FivePer
-    SumTotals = Sumtotals / inputVals.value
-    document.getElementById('result-Amounts').textContent = SumTotals.toFixed(2);
-}
+// event for select tip
+labelSelectTip.forEach((btn) =>
+    btn.addEventListener("click", function(e) {
+        // remove background color
+        removeBackground();
 
-function point2() {
-    Sumtotals = inputVal.value * TenPer
-    SumTotals = Sumtotals / inputVals.value
-    document.getElementById('result-Amounts').textContent = SumTotals.toFixed(2);
-}
+        // add background color
+        // btn.classList.add("add");
+        this.classList.add("add");
 
-function point3() {
-    Sumtotals = inputVal.value * FifteenPer
-    SumTotals = Sumtotals / inputVals.value
-    document.getElementById('result-Amounts').textContent = SumTotals.toFixed(2);
-}
+        // calctip
+        calcTip(btn);
+    })
+);
 
-function point4() {
-    Sumtotals = inputVal.value * TwentyFivePer
-    SumTotals = Sumtotals / inputVals.value
-    document.getElementById('result-Amounts').textContent = SumTotals.toFixed(2);
-}
+// event for custom tip
+document.querySelector("form").addEventListener("submit", function(e) {
+    // prevent default reload
+    e.preventDefault();
 
-function point5() {
-    Sumtotals = inputVal.value * FiftyPer
-    SumTotals = Sumtotals / inputVals.value
-    document.getElementById('result-Amounts').textContent = SumTotals.toFixed(2);
-}
+    // calcTip
+    calcTip(customTip);
+});
 
-//The Formula here is
+// event for reset
+btnReset.addEventListener("click", init);
 
-//For TIP AMOUNT
-// BILL* 0.PERCENT DIVIDED BY THE NUMBER OF PERSONS
-
-// FOR TOTAL AMOUNT
-// BILL* 1.PERCENT DIVIDED BY THE NUMBER OF PERSONS
+// remove btn background color if click outside
+document.addEventListener("click", function(e) {
+    // if click outside remove background
+    if (!e.target.classList.contains("btn")) {
+        // remove background
+        removeBackground();
+    }
+});
